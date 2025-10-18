e71c988cf4a0cc7f0974432a1760e31d0d6663ee
Francis <francisesther194@gmail.com>
2025-10-18 15:22:40 +0100

    recent changes

--- hello.js
+++ hello.js
@@ -1,2 +1,5 @@
-let name = "Ester Ozioma"
-console.log(name)
+
+let name = "Jobinson"
+let course = "Telecom Eng"
+let hobby = "Gaming"
+console.log("Hello, my name is " + name + "and I studied " + course + ". In my free time, I enjoy " + hobby + ".") 
